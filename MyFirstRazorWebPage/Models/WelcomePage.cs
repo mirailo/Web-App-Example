edde7e0e83c622a075892c9c2334d138ec703cd7
mirailo <72035900+mirailo@users.noreply.github.com>
2020-12-09 23:00:22 +0000

    implemented welcome msg

--- MyFirstRazorWebPage/Models/WelcomePage.cs
+++ MyFirstRazorWebPage/Models/WelcomePage.cs
@@ -8,26 +8,16 @@ namespace MyFirstRazorWebPage.Models
         [Required]
         public int ID { get; set; }
 
-        [Required]
-        [Display(Name = "Message Paragraph 1:")]
-        public string Message1 { get; set; }
+      
 
         [Required]
-        [Display(Name = "Message Paragraph 2:")]
-        public string Message2 { get; set; }
-
-        [Required]
-        [Display(Name = "Message Paragraph 3:")]
-        public string Message3 { get; set; }
+        [Display(Name = "Welcome Message:")]
+        public string Message { get; set; }
 
 
         [Required]
         [Display(Name = "Date Update")]
         public string DateUpdate { get; set; }
-
-        [Required]
-        [Display(Name = "User")]
-        public string User { get; set; }
         
     }
 }
--- MyFirstRazorWebPage/Pages/Index.cshtml.cs
+++ MyFirstRazorWebPage/Pages/Index.cshtml.cs
@@ -18,7 +18,7 @@ namespace MyFirstRazorWebPage.Pages
         private readonly HairSalonApptContext _context;
         private readonly IWebHostEnvironment _env;
 
-
+        public string Message { get; set; }
         public IndexModel(ILogger<IndexModel> logger, HairSalonApptContext context, IWebHostEnvironment env)
         {
             _logger = logger;
@@ -28,7 +28,25 @@ namespace MyFirstRazorWebPage.Pages
 
         public IActionResult OnGet()
         {
+            string WelcomeFile = "Welcome.txt";
+            var FileLocation = Path.Combine(_env.WebRootPath, "WelcomeMessage", WelcomeFile);
+            Console.WriteLine("Message file location : " + FileLocation);
 
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(FileLocation))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    Message = line;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
             return Page();
 
         }
